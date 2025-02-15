c9976451aa318877ef9def554e33a9412fb786e1
Arjun <simplegamerofficial97@gmail.com>
2025-02-15 11:07:58 +0530

    added default page to /home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from 'react';
-import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import SideBarNav from './components/sidebar/sidebar-nav';
 import Home from './components/sidebar/sidebar-tabs/home';
 import Project3 from './components/sidebar/sidebar-tabs/projects/project3';
 import Other from './components/sidebar/sidebar-tabs/other';
-import About from './components/sidebar/sidebar-tabs/about';
+// import About from './components/sidebar/sidebar-tabs/about';
 import { InputContext } from './components/sidebar/sidebar-tabs/projects/project3/kanban/input-context';
 import Project1 from './components/sidebar/sidebar-tabs/projects/project1/project1';
 import RecipeApp from './components/sidebar/sidebar-tabs/projects/project2/recipe-app/recipe-app';
@@ -13,24 +12,16 @@ import Favorites from './components/sidebar/sidebar-tabs/projects/project2/recip
 import Details from './components/sidebar/sidebar-tabs/projects/project2/recipe-app/pages/details';
 import FavRecipeDetails from './components/sidebar/sidebar-tabs/projects/project2/recipe-app/pages/favorites/Fav-recipe-details';
 
+
 function App() {
-
-  const [lastVisitedPage, setLastVisitedPage] = useState(localStorage.getItem("lastVisitedPage"));
-  const location = useLocation();
-
-  useEffect(() => {
-    // Update last visited page whenever the user navigates
-    if (location.pathname !== "/project") {
-      setLastVisitedPage(location.pathname);
-      localStorage.setItem("lastVisitedPage", location.pathname);
-    }
-  }, [location]);
 
   return (
       <div className='flex w-full min-h-screen flex-col sm:flex-row sm:w-full overflow-hidden'>
       <InputContext>
       <SideBarNav/>
       <Routes>
+        <Route path='/' element={<Home/>}/>
+        {/* default path to home, when visit site/ */}
         <Route path='/home' element={<Home/>} />
         <Route path='/other' element={<Other/>} />
         {/* <Route path='/about' element={<About/>} /> */}
@@ -44,6 +35,8 @@ function App() {
           </Route>
           <Route path="kanban" element={<Project3 />} />
         </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+        {/* navigate to home when goto url that doesn't exist. ex: home/nsdfsj */}
       </Routes>
       </InputContext>
       </div>
